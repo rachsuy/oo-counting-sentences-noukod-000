274c924782f13f35a3457b53deab5b995a1c2057
Rachele Suy <rachelesuy@gmail.com>
2019-05-22 09:23:19 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,21 +1,24 @@
 require 'pry'
 
-class String
+classclass String
 
-  def sentence?(string)
-    string.end_with?(string-1)
-    
+  def sentence?
+    self.end_with?(".")
   end
 
-  def question?j
-
+  def question?
+    self.end_with?("?")
   end
-
+  
   def exclamation?
-
+    self.end_with?("!")
   end
-
+  
   def count_sentences
-
+    self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
+  
+  def count_sentences_words
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+    
 end
